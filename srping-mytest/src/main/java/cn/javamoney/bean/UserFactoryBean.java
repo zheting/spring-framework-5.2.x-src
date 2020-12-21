3375b7f984f71c056f677f89c00b77d30b6123bd
devin12 <1400902533@qq.com>
2020-12-22 06:49:33 +0800

    Spring IOC 源码阅读

---
+++ srping-mytest/src/main/java/cn/javamoney/bean/UserFactoryBean.java
@@ -0,0 +1,17 @@
+package cn.javamoney.bean;
+
+import org.springframework.beans.factory.FactoryBean;
+
+public class UserFactoryBean implements FactoryBean<User> {
+
+	@Override
+	public User getObject() throws Exception {
+		return new User();
+	}
+
+	@Override
+	public Class<?> getObjectType() {
+		return User.class;
+	}
+}
+
--- srping-mytest/src/main/java/cn/javamoney/test/TestIoc.java
+++ srping-mytest/src/main/java/cn/javamoney/test/TestIoc.java
@@ -1,6 +1,7 @@
 package cn.javamoney.test;
 
 import cn.javamoney.bean.User;
+import cn.javamoney.bean.UserFactoryBean;
 import org.junit.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -14,6 +15,22 @@ public class TestIoc {
 		ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext("beans.xml");
 		User user = ac.getBean("user", User.class);
 		System.out.println(user);
+	}
+
+	/*
+	 *	实现接口FactoryBean
+	 * 	FactoyBean只需要调用getObject就可以返回具体的对象，整个对象的创建过程是由用户自己来控制的
+	 */
+	@Test
+	public void testFactoryBean() {
+		//容器启动的时候，只初始化了UserFactoryBean
+		ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext("beans.xml");
+		//在获取User时，UserFactoryBean才调用getObject获取User,当然第二次获取User时就不会再调用getObject
+		User user = ac.getBean("userFactoryBean", User.class);
+		// 加&获取 UserFactoryBean
+		UserFactoryBean userFactoryBean = ac.getBean("&userFactoryBean", UserFactoryBean.class);
+		System.out.println(user);
+		System.out.println(userFactoryBean);
 	}
 
 
